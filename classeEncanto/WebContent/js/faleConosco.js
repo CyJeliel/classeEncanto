19004871db5756f4d6384177cd027bd7a85af3bb
Helio Campos Mello de Andrade <helio.campos@gmail.com>
2013-09-15 19:07:24 -0300

    Arrumada mensagem de alerta para e-mail e telefone em branco.

--- classeEncanto/WebContent/js/faleConosco.js
+++ classeEncanto/WebContent/js/faleConosco.js
@@ -2,5 +2,5 @@ function check(form) {
 	if ( form.email.value.length != 0 && form.telefone.value.length != 0 )
 		window.location.href = "enviaFaleConosco.jsp";
 	else
-		alert("Erro: Login e senha n\u00E3o podem ficar em branco.");
+		alert("Erro: Email e telefone n\u00E3o podem ficar em branco.");
 }
