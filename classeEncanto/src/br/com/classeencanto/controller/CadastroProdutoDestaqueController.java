552ef0c21faf49a9fde51b1fbb51d17b4fa90138
Cindy de Albuquerque <Cindy de Albuquerque@192.168.0.22>
2013-09-21 22:58:04 -0300

    alterando nome da classe para o padrão do projeto

--- classeEncanto/src/br/com/classeencanto/controller/CadastroProdutoDestaqueController.java
+++ classeEncanto/src/br/com/classeencanto/controller/CadastroProdutoDestaqueController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import br.com.classeencanto.model.impl.Usuario;
 
 @Controller
-public class CadastroProdutoDestaque {
+public class CadastroProdutoDestaqueController {
 
 	@RequestMapping("produtoDestaque")
 	public String login(Usuario usuario) {
