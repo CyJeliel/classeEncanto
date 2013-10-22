7e08e8e9fad93a135c60bb575365666e49228cf3
Cindy de Albuquerque <Cindy de Albuquerque@192.168.0.22>
2013-10-22 09:32:11 -0200

    refatorando para um único produtoController

--- classeEncanto/src/br/com/classeencanto/controller/LoginController.java
+++ classeEncanto/src/br/com/classeencanto/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+	
 	@RequestMapping({ "/login" })
 	public String execute() {
 		return "login";
---
+++ classeEncanto/src/br/com/classeencanto/controller/ProdutoController.java
@@ -0,0 +1,51 @@
+package br.com.classeencanto.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.classeencanto.model.impl.Usuario;
+
+@Controller
+public class ProdutoController {
+
+	@Autowired
+	private AdminController adminController;
+	
+	@RequestMapping("produtoDestaque")
+	public String cadastroProdutosEmDestaque(Usuario usuario) {
+		
+		if (adminController.isLogado()){
+		
+			return "cadastroProdutosEmDestaque";
+		}
+		
+		return "login";
+	}
+
+	@RequestMapping({ "/cadastroDeProduto" })
+	public ModelAndView cadastroDeProduto(String idProduto) {
+		
+		ModelAndView mav = new ModelAndView();
+
+		if (adminController.isLogado()){
+
+			mav.setViewName("cadastroDeProduto");
+        
+		} else {
+		
+			mav.setViewName("login");
+		}
+		
+		return mav;
+
+	}
+	
+	@RequestMapping("listaDeProdutos")
+	public String listaDeProdutos() {
+	
+		return "listaDeProdutos";
+	}
+
+}
