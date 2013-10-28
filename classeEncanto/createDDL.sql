aae3a9e62bb61d4ee438d88e765db649cce72248
Helio Campos Mello de Andrade <helio.campos@gmail.com>
2013-10-28 21:33:34 -0200

    Adding table creation and persistence.

---
+++ classeEncanto/createDDL.sql
@@ -0,0 +1,13 @@
+CREATE TABLE produto (
+	id			SERIAL			PRIMARY KEY				,
+	nome		VARCHAR(20)						NOT NULL,
+	resumo		VARCHAR(50)						NOT NULL,
+	descricao	VARCHAR(50)						NOT NULL
+);
+
+CREATE TABLE usuario (
+	id			SERIAL			PRIMARY KEY				,
+	login		VARCHAR(20)						NOT NULL,
+	senha		VARCHAR(20)						NOT NULL,
+	email		VARCHAR(20)						NOT NULL
+);
---
+++ classeEncanto/dropDDL.sql
@@ -0,0 +1,4 @@
+DROP SEQUENCE produto_id_seq;
+DROP TABLE produto;
+DROP SEQUENCE usuario_id_seq;
+DROP TABLE usuario;
